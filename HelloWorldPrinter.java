9127961408a50172bd734c15a85a4d755a9dc40f
Sneha-Chakraborty <sneha.chakraborty237@gmail.com>
2023-11-09 14:00:20 +0530

    Printer-Code from separate Thread updated.

--- HelloWorldPrinter.java
+++ HelloWorldPrinter.java
@@ -1,5 +1,5 @@
 public class HelloWorldPrinter implements Runnable{
     public void run(){
-        System.out.println("<<<<Hello World>>>> from separate thread!\nCurrent Thread from custom class is : " + Thread.currentThread().getName());
+        System.out.println("<<<<Hello World>>>> from a different thread!\nCurrent Thread from custom class is : " + Thread.currentThread().getName());
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Current Thread from Main method is : " + Thread.currentThread().getName());
+        System.out.println("Current Thread from Main method is :- " + Thread.currentThread().getName());
         HelloWorldPrinter hwp = new HelloWorldPrinter();
         Thread thread = new Thread(hwp);
         thread.start();
